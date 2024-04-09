6c61a65a54b6dc078fd8b367cd03d06efcf58513
wuihee <wuihee@gmail.com>
2024-04-08 19:41:15 -0700

    Updated download_fields script

--- scripts/download_fields.py
+++ scripts/download_fields.py
@@ -7,13 +7,17 @@ API_URL = "https://clinicaltrials.gov/api/v2/studies?pageSize=1000"
 response_json = httpx.get(API_URL).json()
 df = pd.json_normalize(response_json.get("studies"))
 
+# In order to display a progress bar, I need to manually set the number of interations as 492.
 for _ in tqdm.trange(492):
     try:
         next_page_token = response_json.get("nextPageToken")
-        response_json = httpx.get(API_URL + f"&pageToken={next_page_token}").json()
-        df = pd.concat(
-            [df, pd.json_normalize(response_json["studies"])], ignore_index=True
-        )
+        if next_page_token:
+            response_json = httpx.get(API_URL + f"&pageToken={next_page_token}").json()
+            df = pd.concat(
+                [df, pd.json_normalize(response_json["studies"])], ignore_index=True
+            )
+        else:
+            break
     except Exception as e:
         print(f"Encountered erorr {e}.")
         break
